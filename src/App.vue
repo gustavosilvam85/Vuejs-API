2142fd860f8001c9eb29ea14bed9e7e15814fcd4
Gustavo da Silva Moscal <gustavosilvam85@gmail.com>
2024-06-10 14:18:51 -0300

    Projeto Concluido

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-    <h1 v-html="this.question"></h1>
+    <template v-if="this.question">
 
-    <input type="radio" name="options" value="True">
-    <label>True</label><br>
+      <h1 v-html="this.question"></h1>
 
-    <input type="radio" name="options" value="False">
-    <label>False</label><br>
+      <template v-for="(answer, index) in this.answers" :key="index">
+        <input :disabled="this.answerSubmitted" type="radio" name="options" :value="answer" v-model="this.chosen_answer">
+        <label v-html="answer"></label><br>
+      </template>
 
-    <button class="send" type="button">Send</button>
+      <button @click="this.submitAnswer()" class="send" type="button">Send</button>
+      <section v-if="showAnswer">
+        <h2>A resposta é {{ correctAnswer }}</h2>
+      </section>
+    </template>
   </div>
 </template>
 
@@ -21,8 +26,11 @@ export default{
       question: undefined,
       incorrectAnswers: undefined,
       correctAnswer: undefined,
+      chosen_answer: undefined,
+      answerSubmitted:false ,
     }
   },
+
   computed:{
     //faz com que o incorrectAnswer e o correctAnswer seja apenas um array , pois os 2 sao arrays separados
     answers(){
@@ -31,6 +39,21 @@ export default{
       //splice define a posicao da resposta definindo a posicao do array e o math.random * 4 é pra definir qual das posicoes ele vai ficar entre 1 e 4
       answers.splice(Math.random() * 4, 0, this.correctAnswer); 
       return answers;
+    }
+  },
+  methods:{
+    submitAnswer(){
+      this.showAnswer = true;
+      if(!this.chosen_answer){
+        alert('Pick one of the options!');
+      }else{
+        this.answerSubmitted = true;
+        if(this.chosen_answer == this.correctAnswer){
+          alert('You got it right!');
+        }else{
+          alert('You got it wrong!');
+        }
+      }
     }
   },
   //Link para fazer as requisições da API
@@ -64,7 +87,9 @@ export default{
   input[type=radio]{
     margin:12px 4px;
   }
-
+  h2{
+    color: green;
+  }
   button.send{
     margin-top: 12px;
     height:40px;
